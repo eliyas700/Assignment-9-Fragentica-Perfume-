7ce29b716c317704aa0b3c8303632b975121420e
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-04 00:41:04 +0600

    Mini Review Added

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import useReviews from "../../Hooks/useReviews";
+
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const HomePage = () => {
+  // const [reviews, setReviews] = useReviews;
+  const [reviews, setReviews] = useReviews();
+
   return (
     <div>
       <div className="grid grid-cols-[3fr,1fr]">
@@ -16,7 +23,7 @@ const HomePage = () => {
               src="https://fimgs.net/mdimg/dizajneri/o.83.jpg"
               alt=""
             />
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+            <button className="my-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
               Explore Now
             </button>
           </h1>
@@ -25,7 +32,22 @@ const HomePage = () => {
           <img src="https://fimgs.net/mdimg/perfume/375x500.69011.jpg" alt="" />
         </div>
       </div>
-      <div></div>
+      <div>
+        <h4 className="text-3xl font-serif font-bold">Clients Feedbacks....</h4>
+        <div className="grid grid-cols-3">
+          {reviews.slice(0, 3).map((review) => (
+            <ReviewItem key={review.id} item={review} />
+          ))}
+        </div>
+        <Link to="/reviews">
+          <button
+            useNavigate
+            className="mb-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 border border-blue-700 rounded"
+          >
+            See More{" "}
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
---
+++ src/Components/MiniReview/MiniReview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MiniReview = ({ item }) => {
+  return (
+    <div>
+      <h2>Item:{item.name}</h2>
+    </div>
+  );
+};
+
+export default MiniReview;
